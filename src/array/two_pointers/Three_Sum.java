199a7000ccc27ee06a2f52c654ddd8a59f11cbf1
Ankit Rajput <your_email@gmail.com>
2026-04-03 17:22:39 +0530

    Three Sum

--- src/array/two_pointers/Three_Sum.java
+++ src/array/two_pointers/Three_Sum.java
@@ -1,4 +1,77 @@
 package array.two_pointers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+ */
 public class Three_Sum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i < n - 2; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = n - 1;
+            int sum =  -nums[i];
+            while(left < right){
+                int s = nums[left] + nums[right];
+                if(s == sum){
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < n && nums[left] == nums[left - 1]) left++;
+                    while(right >= 0 && nums[right] == nums[right + 1])right--;
+                }
+
+                else if(s < sum) left++;
+                else right--;
+            }
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args){
+        Three_Sum obj = new Three_Sum();
+        int[] arr = {-1, 0, 2, -1, 4, 1};
+        List<List<Integer>> ans = obj.threeSum(arr);
+        System.out.println(ans);
+    }
 }
