da52d0cbb8e2393edb858aa67707de18c706bfb7
Ankit Rajput <your_email@gmail.com>
2026-07-10 17:26:52 +0530

    Update FindPivotIndex and ListNode

--- src/linked_list/ListNode.java
+++ src/linked_list/ListNode.java
@@ -19,5 +19,42 @@ Output: 2
 
  */
 
-public class FindLengthOfLinkedList {
+public class ListNode {
+
+
+    private ListNode next;
+
+    public ListNode(int i) {
+        this.next = null;
+    }
+
+    public ListNode() {
+
+    }
+
+    public int getLength(ListNode head) {
+        // Your code goes here
+        int count = 1;
+        ListNode fast = head;
+        if(head == null) return 0;
+        if(head.next == null) return 1;
+
+        while(fast.next != null){
+            fast = fast.next;
+            count++;
+        }
+        return count;
+    }
+
+        public static void main(String[] args) {
+            ListNode obj = new ListNode();
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+
+            int ans = obj.getLength(head);
+            System.out.println(ans);
+        }
 }
